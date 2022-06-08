1f282d591f700a30d50c0ee9b7765ca9ef4aed1d
imanirak <karinadgarces@gmail.com>
2022-06-07 22:22:39 -0400

    title color

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ import { projects } from "../data";
 
 export default function Projects() {
   return (
-    <section id="projects" className="text-gray-400 bg-amber-400 body-font">
+    <section id="projects" className="text-red-500 bg-amber-400 body-font">
     <div className="container px-5 py-10 mx-auto font-serif text-center lg:px-40">
       <div className="flex flex-col w-full mb-20">
         <CodeIcon className="mx-auto inline-block w-10 mb-4 text-yellow-100" />
